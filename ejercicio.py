5c1e2e800be9e2a03405fbb68e5ff5b7360b1490
carek2025 <1560977@senati.pe>
2025-02-13 19:40:42 -0500

    Mi ejercio parte 6

--- ejercicio.py
+++ ejercicio.py
@@ -3,4 +3,7 @@ b=int(input("ingrese el segundo numero"))
 print(f"la suma es {a+b}")
 print(f"la resta es {a-b}")
 print(f"la multiplicacion es {a*b}")
-print(f"la division es {a/b}")
+if b>0:
+    print(f"la division es {a/b}")
+else:
+    print(f"No se puede dividir entre 0")
